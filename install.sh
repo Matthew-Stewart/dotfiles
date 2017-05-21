c9355add2ee81f6ebb876b98e61d3b7cae0c8788
Matthew Stewart <mstewa09@calpoly.edu>
2017-05-21 03:33:13 -0700

    Added fixes for vim in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# vim complains if these directories don't exist
+mkdir $HOME/dotfiles/.vim/swaps
+mkdir $HOME/dotfiles/.vim/backups
+mkdir $HOME/dotfiles/.vim/undo
+
+# install vundle
+# comment out this line (and vundle section of vimrc) if unneeded
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/dotfiles/.vim/bundle/Vundle.vim
+
+# install powerline
+# assumes 'brew install python' has been run
+# again, comment out as needed
+pip install powerline-status
+
 for file in .{aliases,bash_profile,exports,functions,gitconfig,gitignore_global,path,vimrc}; do
    if [[ -e $HOME/$file ]]; then
       echo "found old ${file}, backing up to ${file}.backup"
